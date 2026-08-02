aa7da46bb601a8bae4f4b98c6ba6dbf0214664d9
emin atilgan <eminatilgan@emins-MacBook-Pro.local>
2026-08-02 15:20:06 +0100

    Update vite config to include industrial.html in build

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         main: 'index.html',
         crank_privacy: 'Crank_Privacy.html',
         pricing: 'pricing.html',
+        industrial: 'industrial.html',
       },
     },
   },
